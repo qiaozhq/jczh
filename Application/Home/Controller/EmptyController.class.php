1c31c29cab8077dc8f4e823f91f577aa91fac080
qiaozhq <63040774@qq.com>
2018-05-05 10:46:18 +0800

    third commit

--- Application/Home/Controller/EmptyController.class.php
+++ Application/Home/Controller/EmptyController.class.php
@@ -6,7 +6,7 @@ class EmptyController extends Controller{
     //空操作_empty()方法
     function _empty(){
         header("HTTP/1.0 404 Not Found");
-        $this->display("Index/error");
+        $this->display("Index/error"); 
     }
     
     function index(){//控制器不存在的时候

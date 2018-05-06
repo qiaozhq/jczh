e96f2a4fdfa1c01fc4c033b6300e553be20f7d09
qiaozhq <63040774@qq.com>
2018-05-07 01:35:33 +0800

    third commit

--- Application/Admin/Controller/AdminuserController.class.php
+++ Application/Admin/Controller/AdminuserController.class.php
@@ -8,71 +8,45 @@ use Think\Exception;
  * @author  Alexander
  */
 class AdminuserController extends CommonController { 
-    //管理员用户管理首页
-    public function index() {
-        $admins = D('Admin')->getAdminData('admin', 'admin_id');
-        $this->assign('admins', $admins);
-        $this->display();
-    }
-
-    //增加管理员用户
-    public function add() {
-        // 保存数据
-        if($_POST) {
-            if(!isset($_POST['username']) || !$_POST['username']) {
-                return show(0, '用户名不能为空');
-            }
-            if(!isset($_POST['password']) || !$_POST['password']) {
-                return show(0, '密码不能为空');
-            }
-            $_POST['password'] = getMd5Password($_POST['password']);
-            // 判定用户名是否存在
-            $admin = D("Admin")->getAdminByUsername($_POST['username']);
-            if($admin && $admin['status']!=-1) {
-                return show(0,'该用户存在');
-            }
-            if($_POST['admin_id']) {
-                return $this->save($_POST);
-            }
-            // 新增
-            $id = D("Admin")->insert('admin',$_POST);
-            if(!$id) {
-                return show(0, '新增失败');
-            }
-            return show(1, '新增成功');
-        }
-        $this->display();
-    }
 
     //个人中心-个人情报取得
-    public function personal() {
+    public function per() {
         $admin = $this->getLoginUser();
         $this->assign('admin',$admin);
         $this->display();
     }
 
     //个人中心-个人情报保存
-    public function save($data) {
+    public function chg() {
         $user = $this->getLoginUser();
         if(!$user) {
             return show(0,'用户不存在');
         }
-        $id = $data['admin_id'];
-        unset($data['admin_id']);
+        if($_POST['oldpas']) {
+            if($user['password'] != getMd5Password($_POST['oldpas'])) {
+                return show(0,'密码错误');
+            }
+            $_POST['password'] = getMd5Password($_POST['newpas']);
+        }
+        $id = $_POST['admin_id'];
+        unset($_POST['admin_id']);
         try {
-            $result = D("Admin")->updateDataById('admin', $id, $data, 'admin_id');
+            $result = D("Admin")->updateDataById('admin', $id, $_POST, 'admin_id');
             if($result === false) {
                 return show(0,'更新失败');
             }
+            $ret = D('Admin')->getAdminByUsername($_POST['username']);
+            session('adminUser', $ret);
             return show(1,'更新成功');
         }catch(Exception $e) {
             return show(0,$e->getMessage());
         }
     }
 
-    //启用/禁用管理员用户
-    public function setStatus() {
-        return parent::setStatus($_POST,'Admin');    
+    //个人中心-个人情报取得 修改密码画面
+    public function pas() {
+        $admin = $this->getLoginUser();
+        $this->assign('admin',$admin);
+        $this->display();
     }
-
 }
